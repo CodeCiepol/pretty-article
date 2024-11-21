2127e025ac957af0259014d58aa202d23685fef5
Ciepol <adamulicho@gmail.com>
2024-11-21 13:53:59 +0100

    Added template, view to formatted article

--- __main__.py
+++ __main__.py
@@ -3,32 +3,46 @@ import json
 import openai
 from openai import OpenAI
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
 
-def get_article_from_file(filename: str):
+def get_file(filename: str):
     with open(filename, "r", encoding="utf-8") as file:
-        article_str = file.read()
-    return article_str
+        file_str = file.read()
+    return file_str
 
 
-def save_article(content: str, filename: str):
+def save_file(content: str, filename: str):
     with open(f"{filename}", "w", encoding="utf-8") as file:
         file.write(content)
+
+
+def generate_view_from_template(article_html, template_name="szablon.html"):
+    template_html = get_file(template_name)
+
+    template_soup = BeautifulSoup(template_html, "html.parser")
+
+    if template_soup.body:
+        template_soup.body.clear()
+        template_soup.body.append(article_html)
+        save_file(str(template_soup.prettify()), "podglad.html")
 
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 if not API_KEY:
-    raise EnvironmentError("Missing API key. Ensure that the .env file contains the correct API_KEY variable.")
+    raise EnvironmentError(
+        "Missing API key. Ensure that the .env file contains the correct API_KEY variable."
+    )
 
 output_language = "Polish"
 model = "gpt-4o-mini"
 
-article_str = get_article_from_file("artykul.txt")
+article_str = get_file("artykul.txt")
 
 prompt = f'Use appropriate HTML tags to structure given article content. Follow these guidelines:\n \
-1. Use semantic HTML tags such as <h1>, <h2>, <p>, <ul>, <ol> to properly organize the content.\n\
+1. Use semantic HTML tags such as <article>, <h1>, <h2>, <p>, <ul>, <ol> to properly organize the content.\n\
 2. Indicate places where images should be included by using the <img> tag with the attribute src="image_placeholder.jpg".\n\
 3. Add an alt attribute to each <img> tag that provides a detailed prompt for generating the image.\n\
 4. Include captions under the images using the <figcaption> tag, wrapping <img> and <figcaption> within a <figure> tag.\n\
@@ -79,5 +93,8 @@ if not response.choices:
 
 message_content = response.choices[0].message.content
 response_dict = json.loads(message_content)
+formatted_article_str = response_dict["article"]
 
-save_article(response_dict["article"], "artykul.html")
+save_file(formatted_article_str, "artykul.html")
+
+generate_view_from_template(BeautifulSoup(formatted_article_str, "html.parser"))
